4df96d24474b475bdff56a332a2f6468ff3f51fe
Scarlet Villasana <s.claret.v@gmail.com>
2019-08-15 15:26:23 -0400

    avance index y otros

---
+++ index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<?php  $version = date('YmdHis'); ?>
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<title>Mavimovil</title>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, user-scalable=no">
+	<meta name="description" content="Autofin"> 
+
+
+	<link rel="stylesheet" href="assets/web/css/bootstrap.min.css?<?php echo $version ?>">
+	<link rel="stylesheet" href="assets/web/css/animate.css?<?php echo $version ?>">
+	
+	<link rel="stylesheet" href="assets/web/css/main.css?<?php echo $version ?>">
+
+
+	<script src="assets/web/js/vendor/jquery-1.11.2.min.js?<?php echo $version ?>"></script>
+	<script src="assets/web/js/vendor/bootstrap.min.js?<?php echo $version ?>"></script>
+
+</head>
+<body>
+	<section>
+		<!-- navbar -->
+		<?php include 'menu.php'  ?>
+	</section>
+
+	<section class="section-1">
+
+		<div class="sliders-index slider1 ">
+			<div class="row">
+				<div class="col-lg-6">
+					<h1 class="heading-1"><span class="text-span">Hacemos posible la app y la plataforma web que necesitas</span></h1><p class="paragraph-1">Diseñamos, desarrollamos e integramos la tecnologia que requieras.</p>
+					<a href="#" class="button-1"><span>Ver más</span></a>
+					
+					<div class="sliders-index-controls">
+					    <ul class="list-inline">
+					      <li class="list-inline-item active">
+					        <a  id="" class="list-inline-a">Apps</a>
+					      </li>
+					      <li class="list-inline-item">
+					         <a  id="" class="list-inline-a">Plataformas</a>
+					      </li>
+					      <li class="list-inline-item">
+					         <a  id="" class="list-inline-a">Integración</a>
+					      </li>
+					    </ul>
+					</div>
+				</div>
+				<div class="col-lg-6"></div>
+			</div>
+		</div>
+
+		<div class="sliders-index slider2 hide" >
+			<div class="row">
+				<div class="col-lg-6">
+					<h1 class="heading-1"><span class="text-span">Hacemos posible la app y la plataforma web que necesitas</span></h1><p class="paragraph-1">Diseñamos, desarrollamos e integramos la tecnologia que requieras.</p>
+					<a href="#" class="button-1"><span>Ver más</span></a>
+
+					<div class="sliders-index-controls">
+					    <ul class="list-inline">
+					      <li class="list-inline-item active">
+					        <a  id="" class="list-inline-a">Apps</a>
+					      </li>
+					      <li class="list-inline-item">
+					         <a  id="" class="list-inline-a">Plataformas</a>
+					      </li>
+					      <li class="list-inline-item">
+					         <a  id="" class="list-inline-a">Integración</a>
+					      </li>
+					    </ul>
+					</div>
+				</div>
+				<div class="col-lg-6"></div>
+			</div>
+		</div>
+
+		<div class="sliders-index slider3 hide" >
+			<div class="row">
+				<div class="col-lg-6">
+					<h1 class="heading-1"><span class="text-span">Hacemos posible la app y la plataforma web que necesitas</span></h1><p class="paragraph-1">Diseñamos, desarrollamos e integramos la tecnologia que requieras.</p>
+					<a href="#" class="button-1"><span>Ver más</span></a>
+
+					<div class="sliders-index-controls">
+					    <ul class="list-inline">
+					      <li class="list-inline-item active">
+					        <a  id="" class="list-inline-a">Apps</a>
+					      </li>
+					      <li class="list-inline-item">
+					         <a  id="" class="list-inline-a">Plataformas</a>
+					      </li>
+					      <li class="list-inline-item">
+					         <a  id="" class="list-inline-a">Integración</a>
+					      </li>
+					    </ul>
+					</div>
+				</div>
+				<div class="col-lg-6"></div>
+			</div>
+		</div>		
+
+	</section>
+
+	<!--<div class="section"><div class="container-5 w-container"><div class="columns w-row"><div class="column w-col w-col-7"><h1 class="heading-3"><span class="text-span">Hacemos posible la app y la plataforma web que necesitas</span></h1><p class="paragraph">Diseñamos, desarrollamos e integramos la tecnologia que requieras.</p><a href="#" class="button w-button"><span>Ver</span> más</a></div><div class="column-2 w-col w-col-5"><img src="https://uploads-ssl.webflow.com/5cc46826d9da3376e27ebde0/5d54a260ed71c94635a98bc0_slider-elemento-1.png" alt="" class="image"/></div></div></div><img src="https://uploads-ssl.webflow.com/5cc46826d9da3376e27ebde0/5d549bf61d1d7710c0d3a4b5_slider-1.png" srcset="https://uploads-ssl.webflow.com/5cc46826d9da3376e27ebde0/5d549bf61d1d7710c0d3a4b5_imagen1-p-500.png 500w, https://uploads-ssl.webflow.com/5cc46826d9da3376e27ebde0/5d549bf61d1d7710c0d3a4b5_slider-1.png 1320w" sizes="(max-width: 1320px) 100vw, 1320px" alt=""/></div>-->
+
+	<!--<div class="section"><div class="w-container"><div class="columns-2 w-row"><div class="w-col w-col-6"><div class="div-block-4"><div class="div-block-2"><img src="https://uploads-ssl.webflow.com/5cc46826d9da3376e27ebde0/5d5564f5548be46a35dc73dd_logotipo-mavimovil.svg" alt=""/></div><p class="paragraph-2">Somos Mavimovil, una empresa que caracteriza en desarrollar soluciones tecnológicas a medida, integrando los entornos tecnológicos que nuestros clientes utiliza. Somos Mavimovil, una empresa que caracteriza en desarrollar soluciones tecnológicas a medida, integrando los entornos tecnológicos que nuestros clientes utiliza. Somos Mavimovil, una empresa que caracteriza en desarrollar soluciones tecnológicas a medida, integrando los entornos tecnológicos que nuestros clientes utiliza. </p></div></div><div class="column-3 w-col w-col-6"><div class="text-block">Los mercados en los cuales hemos creado son:</div><div class="w-layout-grid grid"><img src="https://uploads-ssl.webflow.com/5cc46826d9da3376e27ebde0/5d55687a4d608ad2922f59fe_icono-1-mercados.svg" alt=""/><img src="https://uploads-ssl.webflow.com/5cc46826d9da3376e27ebde0/5d55687a4d608a86192f59ff_icono-2-mercados.svg" alt=""/><img src="https://uploads-ssl.webflow.com/5cc46826d9da3376e27ebde0/5d55687aed71c9a6dbad6cc4_icono-3-mercados.svg" alt=""/><img src="https://uploads-ssl.webflow.com/5cc46826d9da3376e27ebde0/5d55687afee1f9cd2695a741_icono-4-mercados.svg" alt=""/><img src="https://uploads-ssl.webflow.com/5cc46826d9da3376e27ebde0/5d55687af55383451bdae842_icono-5-mercados.svg" alt=""/><img src="https://uploads-ssl.webflow.com/5cc46826d9da3376e27ebde0/5d55687bf55383714edae843_icono-7-mercados.svg" alt=""/><img src="https://uploads-ssl.webflow.com/5cc46826d9da3376e27ebde0/5d55687bed71c979f8ad6cc5_icono-8-mercados.svg" alt=""/><img src="https://uploads-ssl.webflow.com/5cc46826d9da3376e27ebde0/5d55687b4d608ab4c32f5a00_icono-6-mercados.svg" alt=""/></div></div></div></div></div><div class="section"><div class="w-container"><div class="columns-3 w-row"><div class="w-col w-col-6"><img src="https://uploads-ssl.webflow.com/5cc46826d9da3376e27ebde0/5d556dbffee1f9862a95db26_apps-fondo-1.svg" alt="" class="image-7"/><img src="https://uploads-ssl.webflow.com/5cc46826d9da3376e27ebde0/5d556dbff55383f41bdb3292_apps-fondo-2.svg" alt="" class="image-8"/></div><div class="w-clearfix w-col w-col-6"><img src="https://uploads-ssl.webflow.com/5cc46826d9da3376e27ebde0/5d556adeed71c97a7fad94d8_IconoApp.svg" width="150" alt="" class="image-6"/><h1 class="heading">Desarrollo de Apps</h1><h3 class="heading-2">Aplicaciones móviles nativas</h3><p class="paragraph-3">Somos Mavimovil, una empresa que caracteriza en desarrollar soluciones tecnológicas a medida, integrando los entornos tecnológicos que nuestros clientes utiliza. Somos Mavimovil, una empresa que caracteriza en desarrollar soluciones tecnológicas a medida, integrando los entornos tecnológicos que nuestros clientes utiliza. Somos Mavimovil, una empresa que caracteriza en desarrollar soluciones tecnológicas a medida, integrando los entornos tecnológicos que nuestros clientes utiliza. </p></div></div></div></div><div class="section"><div class="w-container"><div class="columns-4 w-row"><div class="w-col w-col-1"></div><div class="w-col w-col-10"></div><div class="w-col w-col-1"></div></div></div><div class="w-container"><div class="w-row"><div class="w-col w-col-1"></div><div class="w-col w-col-10"></div><div class="w-col w-col-1"></div></div></div></div><div class="section"><div></div><div class="div-block-3"><div class="w-container"><div class="w-row"><div class="w-col w-col-6"><h1>Heading</h1></div><div class="w-col w-col-6"></div></div></div></div><div class="div-block-3"><div class="w-row"><div class="w-col w-col-1"></div><div class="w-col w-col-10"></div><div class="w-col w-col-1"></div></div><div class="w-row"><div class="w-col w-col-1"></div><div class="w-col w-col-10"></div><div class="w-col w-col-1"></div></div></div><div></div></div>--><script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.4.1.min.220afd743d.js" type="text/javascript" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script><script src="https://uploads-ssl.webflow.com/5cc46826d9da3376e27ebde0/js/webflow.5ab29a3fc.js" type="text/javascript"></script><!--[if lte IE 9]><script src="//cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif]--></body></html>
---
+++ main.php
@@ -0,0 +1,63 @@
+   <div class="box-y" >
+
+                  
+
+                  <div class="controles">
+
+                    <ul class="list-inline">
+                      <li class="list-inline-item btn-auto active">
+                        <a  id="control-auto"> <img src="assets/icons/autofin-icono-web-1.svg"  width="30" height="30" alt=""  aria-hidden=true></a>
+                      </li>
+                      <li class="list-inline-item btn-moto">
+                         <a  id="control-moto"><img src="assets/icons/autofin-icono-web-2.svg"  width="30" height="30" alt=""  aria-hidden=true></a>
+                      </li>
+                    </ul>
+                </div>
+
+
+                <div id="box-y-auto">
+                  
+                  <img src="assets/img/auto.png"   class="auto ">
+
+                  <div class="contenido-box-y animated fadeIn">
+
+                    <h4 class="titulo-box-y">
+
+                      ¡TIENES EL AUTO QUE NUESTROS CLIENTES QUIEREN Y NOSOTROS EL MEJOR FINANCIAMIENTO Y TECNOLOGÍA!</h4>
+
+                    <p class="text-box-y">
+
+                    En Autofin sabemos el auto que necesitan nuestros clientes y que tú puedes ofrecerles, es por eso que ponemos a tu alcance todos nuestros servicios tecnológicos y las mejores alternativas en financiamiento.</p>
+
+                    <a href="#" class="btn button-box-y btn-comencemos">
+
+                    COMENCEMOS</a>
+
+                  </div>
+
+                </div>
+
+                 <div id="box-y-moto" class="hide">
+                  
+                  <img src="assets/img/MOTO.png"   class="moto ">
+
+                  <div class="contenido-box-y animated fadeIn">
+
+                    <h4 class="titulo-box-y">
+
+                      ¡TIENES LA MOTO QUE NUESTROS CLIENTES QUIEREN Y NOSOTROS EL MEJOR FINANCIAMIENTO Y TECNOLOGÍA!</h4>
+
+                    <p class="text-box-y">
+
+                    En Autofin sabemos la moto que necesitan nuestros clientes y que tú puedes ofrecerles, es por eso que ponemos a tu alcance todos nuestros servicios tecnológicos y las mejores alternativas en financiamiento.</p>
+
+                    <a href="#" class="btn button-box-y btn-comencemos">
+
+                    COMENCEMOS</a>
+
+                  </div>
+
+                </div>
+            </div>
+                
+
