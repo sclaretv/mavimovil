05abdf5ec785265693ca92ec7c90500ca0794f30
Scarlet Villasana <s.claret.v@gmail.com>
2019-08-16 15:19:50 -0400

    avances

---
+++ clientes.php
@@ -0,0 +1,17 @@
+<div class="container">
+			<div class="row">
+				<h1 class="heading-4">Confían en nosotros</h1>
+				<p class="paragraph-4">Somos Mavimovil una empresa que caracteriza en desarrollar soluciones<br> tecnológicas a medida, integrando los entornos tecnológicos.</p>
+			</div>
+
+			<div class="carousel-confian">
+			    <div><img class="imagen-confian" alt="Mavimovil" src="assets/web/img/clientes_Mesa de trabajo 1.png"  height="50"></div>
+			    <div><img class="imagen-confian" alt="Mavimovil" src="assets/web/img/clientes-02.png"  height="50"></div>
+			    <div><img class="imagen-confian" alt="Mavimovil" src="assets/web/img/clientes-03.png"  height="50"></div>
+			    <div><img class="imagen-confian" alt="Mavimovil" src="assets/web/img/clientes-04.png"  height="50"></div>
+			    <div><img class="imagen-confian" alt="Mavimovil" src="assets/web/img/clientes-05.png"  height="50"></div>
+			    <div><img class="imagen-confian" alt="Mavimovil" src="assets/web/img/clientes-06.png"  height="50"></div>
+			    <div><img class="imagen-confian" alt="Mavimovil" src="assets/web/img/clientes-07.png"  height="50"></div>
+			</div>
+			
+		</div>	
---
+++ contacto.php
@@ -0,0 +1,59 @@
+<div class="container ">
+			<div class="row">
+				<div class="col-lg-6">
+					<div>
+						<h1 class="heading-5">¿Alguna necesidad que cubrir?</h1>
+					</div>
+					<div>
+						<p class="paragraph-5">Somos Mavimovil una empresa que caracteriza en desarrollar soluciones tecnológicas a medida, integrando los entornos tecnológicos.</p>
+					</div>
+				</div>
+
+				<div class="col-lg-6">
+					
+					<form method="post" action="/form/insert/home" id="form_contact">
+
+						<div class="form-group">
+						    <input type="text" class="form-control" id="nombre" name="contacto_nombre"  required="" placeholder="Nombre">
+						</div>
+
+						<div class="form-group">
+						    <input type="text" class="form-control" id="empresa" name="contacto_empresa" placeholder="Empresa">
+						</div>
+
+						<div class="form-group">
+						    <input type="text" class="form-control" id="fono" name="contacto_fono" placeholder="Fono"  >
+						</div> 
+
+						<div class="form-group">
+						    <input type="text" class="form-control" id="email" name="contacto_email" placeholder="Email" required="">
+						</div> 
+
+						<div class="form-group">
+						    <textarea class="form-control" id="mensaje" name="contacto_mensaje"  required="" placeholder="Mensaje"></textarea>
+						</div> 
+
+						<!-- FORMULARIO CON RECAPTCHA -->
+						<div class="g-recaptcha" data-sitekey="6LeMkaAUAAAAAJI_ctx_3G_j3kWXZeim4C3uiqix"></div> 
+
+						<div class="text-center">
+							<div class="alert alert-warning" style="display:none">
+								 
+							</div>
+							<div class="alert alert-success" style="display:none">
+								 Has enviado tu mensaje con éxito. Gracias.
+							</div>
+							<div class="alert alert-info loading" style="display:none">
+								Espere un momento ...
+							</div>
+						</div>
+						 
+						<div class="text-right">
+							<button type="submit" class="btn-contact"> <strong>Enviar</strong> </button>
+						</div>
+
+					</form>
+					
+				</div>
+			</div>
+		</div>
