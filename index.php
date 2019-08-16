e776fa74d3e0b7be6ecf3de2b0a4c5622cd312a6
Scarlet Villasana <s.claret.v@gmail.com>
2019-08-16 15:53:59 -0400

    avances

--- index.php
+++ index.php
@@ -52,23 +52,96 @@
 	<section class="section-6" >
 		<?php //include 'tecnologias.php'  ?>
 		<div class="container">
-	<div class="row">
-		<div class="col-lg-6">
-			<div class="text-left">
-				<img alt="Desarrollo de Apps" src="assets/web/img/IconoPlataforma.svg" height="200">
+			<div class="row">
+				<div class="col-lg-6">
+					<div class="text-left">
+						<img alt="Desarrollo de Apps" src="assets/web/img/IconoPlataforma.svg" height="200">
+					</div>
+					<div>
+						<h1 class="heading-6">Desarrollo de <br>Plataformas</h1>
+						<p class="paragraph-7">Somos Mavimovil, una empresa que caracteriza en desarrollar soluciones tecnológicas a medida, integrando los entornos tecnológicos que nuestros clientes utiliza. Somos Mavimovil, una empresa que caracteriza en desarrollar soluciones tecnológicas a medida, integrando los entornos tecnológicos que nuestros clientes utiliza. Somos Mavimovil, una empresa que caracteriza en desarrollar soluciones tecnológicas a medida, integrando los entornos tecnológicos que nuestros clientes utiliza.</p>
+					</div>
+				</div>
+
+				<div class="col-lg-6">
+					
+				</div>
+
 			</div>
-			<div>
-				<h1 class="heading-6">Desarrollo de <br>Plataformas</h1>
-				<p class="paragraph-7">Somos Mavimovil, una empresa que caracteriza en desarrollar soluciones tecnológicas a medida, integrando los entornos tecnológicos que nuestros clientes utiliza. Somos Mavimovil, una empresa que caracteriza en desarrollar soluciones tecnológicas a medida, integrando los entornos tecnológicos que nuestros clientes utiliza. Somos Mavimovil, una empresa que caracteriza en desarrollar soluciones tecnológicas a medida, integrando los entornos tecnológicos que nuestros clientes utiliza.</p>
+
+			<div class="row">
+			<div id="carousel-apps" class="carousel slide" data-ride="carousel">
+			  <!-- Indicators -->
+			  <ol class="carousel-indicators">
+			    <li data-target="#carousel-apps" data-slide-to="0" class="active"></li>
+			    <li data-target="#carousel-apps" data-slide-to="1"></li>
+			    <li data-target="#carousel-apps" data-slide-to="2"></li>
+			    <li data-target="#carousel-apps" data-slide-to="3"></li>
+			    <li data-target="#carousel-apps" data-slide-to="4"></li>
+			    <li data-target="#carousel-apps" data-slide-to="5"></li>
+			    <li data-target="#carousel-apps" data-slide-to="6"></li>
+			    <li data-target="#carousel-apps" data-slide-to="7"></li>
+			    <li data-target="#carousel-apps" data-slide-to="8"></li>
+			    <li data-target="#carousel-apps" data-slide-to="9"></li>
+			  </ol>
+
+			  <!-- Wrapper for slides -->
+			  <div class="carousel-inner" role="listbox">
+			    <div class="item active">
+			      <img src="..." alt="...">
+			      
+			    </div>
+			    <div class="item">
+			      <img src="..." alt="...">
+			      
+			    </div>
+			    <div class="item">
+			      <img src="..." alt="...">
+			      
+			    </div>
+			    <div class="item">
+			      <img src="..." alt="...">
+			      
+			    </div>
+			    <div class="item">
+			      <img src="..." alt="...">
+			      
+			    </div>
+			    <div class="item">
+			      <img src="..." alt="...">
+			      
+			    </div>
+			    <div class="item">
+			      <img src="..." alt="...">
+			      
+			    </div>
+			    <div class="item">
+			      <img src="..." alt="...">
+			      
+			    </div>
+			    <div class="item">
+			      <img src="..." alt="...">
+			      
+			    </div>
+			    <div class="item">
+			      <img src="..." alt="...">
+			      
+			    </div>
+			    
+			  </div>
+
+			  <!-- Controls -->
+			  <a class="left carousel-control" href="#carousel-apps" role="button" data-slide="prev">
+			    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+			    <span class="sr-only">Previous</span>
+			  </a>
+			  <a class="right carousel-control" href="#carousel-apps" role="button" data-slide="next">
+			    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+			    <span class="sr-only">Next</span>
+			  </a>
+			</div>
 			</div>
 		</div>
-
-		<div class="col-lg-6">
-			
-		</div>
-
-	</div>
-</div>
 	</section>
 
 
