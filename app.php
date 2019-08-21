a717802a0fb6233354f74367b8f0b1d8e71b4710
Scarlet Villasana <s.claret.v@gmail.com>
2019-08-21 00:41:31 -0400

    avances

--- app.php
+++ app.php
@@ -1,10 +1,27 @@
 <div class="container">
 	<div class="row">
-		<div class="col-lg-6">
+		<div class="col-lg-7">
+			<div>
+				<img src="/assets/web/img/apps-fondo-2.svg" class="fondo4" alt=""  aria-hidden=true>
+				<img src="/assets/web/img/fondo-verde.svg" alt=""  class="fondo5" aria-hidden=true>
+
+				<div class="animated  fast3 efx" data-up="fadeInLeft" data-down="fadeOut "  style="z-index: 2">
+					<img src="/assets/web/img/app-3.png" alt=""  class="fondo6" aria-hidden=true>
+				</div>
+				
+				<div class="animated fast2 efx" data-up="fadeInLeft" data-down="fadeOut "  style="z-index: 3">
+					<img src="/assets/web/img/app-2.png" alt=""  class="fondo7" aria-hidden=true>
+				</div>
+
 			
+
+				<div class="animated fast efx"  data-up="fadeInLeft" data-down="fadeOut " style="z-index: 4">
+					<img src="/assets/web/img/app-1.png" alt=""  class="fondo8" aria-hidden=true>
+				</div>
+			</div>
 		</div>
 
-		<div class="col-lg-6 container-3">
+		<div class="col-lg-5 ">
 			<div class="text-right">
 				<img alt="Desarrollo de Apps" src="/assets/web/img/IconoApp.svg" width="300" height="200" class="efx" data-up="fadeInRight" data-down="fadeOut ">
 			</div>
