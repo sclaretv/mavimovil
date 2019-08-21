76ed9ca266ff47c96c48c09209bd5286a610d0ac
Scarlet Villasana <s.claret.v@gmail.com>
2019-08-20 20:17:35 -0400

    avances

--- menu.php
+++ menu.php
@@ -24,12 +24,15 @@
         <li class="nav-item"><a class="nav-link" href="#clientes">Clientes</a></li>
         <li class="nav-item"><a class="nav-link" href="#contacto">Contacto</a></li>
 
-        <li class="nav-item dropdown">
-          <a class="nav-link af-link" href="es/index.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><img class="img-idioma" alt="Español" src="/assets/web/img/espanol.svg"> <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li class="nav-item"><a class="nav-link af-link" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><img class="img-idioma" alt="Ingles" src="/assets/web/img/ingles.svg"> <span class="caret"></span></a></li>
-          </ul>
-        </li>
+        <div class="idioma">
+          <li class="nav-item dropdown">
+            <a class="nav-link af-link" href="es/index.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><img class="" alt="Español/Spanish" src="/assets/web/img/espanol.svg"> <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li class="nav-item"><a class="nav-link af-link" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><img class="" alt="English/Inglés" src="/assets/web/img/ingles.svg"> <span class="caret"></span></a></li>
+            </ul>
+          </li>
+        </div>
+        
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
