607ccc6ba52f02c041c81b1cf3b9e41698a236a0
Scarlet Villasana <s.claret.v@gmail.com>
2019-08-22 23:26:26 -0400

    avances

--- tecnologias.php
+++ tecnologias.php
@@ -5,13 +5,13 @@
 			</div>
 
 			<div class="carousel-tecnologias">
-			    <div><img class="" alt="Cordova" src="assets/web/img/tecnologias_01.png"  height="200"></div>
-			    <div><img class="" alt="Ionic" src="assets/web/img/tecnologias_02.png"  height="200"></div>
-			    <div><img class="" alt="IOS" src="assets/web/img/tecnologias_03.png"  height="200"></div>
-			    <div><img class="" alt="React" src="assets/web/img/tecnologias_04.png"  height="200"></div>
-			    <div><img class="" alt="Android" src="assets/web/img/tecnologias_05.png"  height="200"></div>
-			    <div><img class="" alt="SQLite" src="assets/web/img/tecnologias_06.png"  height="200"></div>
-			    <div><img class="" alt="Xamarin" src="assets/web/img/tecnologias_07.png"  height="200"></div>
-			    <div><img class="" alt="JavaScript" src="assets/web/img/tecnologias_08.png"  height="200"></div>
+			    <div><img class="carousel-tecnologias-img" alt="Cordova" src="assets/web/img/tecnologias_01.png"  height="180"></div>
+			    <div><img class="carousel-tecnologias-img" alt="Ionic" src="assets/web/img/tecnologias_02.png"  height="180"></div>
+			    <div><img class="carousel-tecnologias-img" alt="IOS" src="assets/web/img/tecnologias_03.png"  height="180"></div>
+			    <div><img class="carousel-tecnologias-img" alt="React" src="assets/web/img/tecnologias_04.png"  height="180"></div>
+			    <div><img class="carousel-tecnologias-img" alt="Android" src="assets/web/img/tecnologias_05.png"  height="180"></div>
+			    <div><img class="carousel-tecnologias-img" alt="SQLite" src="assets/web/img/tecnologias_06.png"  height="180"></div>
+			    <div><img class="carousel-tecnologias-img" alt="Xamarin" src="assets/web/img/tecnologias_07.png"  height="180"></div>
+			    <div><img class="carousel-tecnologias-img" alt="JavaScript" src="assets/web/img/tecnologias_08.png"  height="180"></div>
 			</div>
 		</div>
