0c738085996ca5d1d2deff513f3a5840f0063d56
Scarlet Villasana <s.claret.v@gmail.com>
2019-08-25 22:53:56 -0400

    avances responsive

--- clientes.php
+++ clientes.php
@@ -1,16 +1,35 @@
 <div class="container">
 			<div class="row">
-				<h1 class="heading-4">Confían en nosotros</h1>
-				<p class="paragraph-4">Cada uno de nuestros proyectos ha cambiado la vida de estas empresas.</p>
+				<div class="col-md-12 col-md-offset-0 col-sm-6 col-sm-offset-3 ">
+					<h1 class="heading-4">Confían en nosotros</h1>
+					<p class="paragraph-4">Cada uno de nuestros proyectos ha cambiado la vida de estas empresas.</p>
+				</div>
 			</div>
 
 			<div class="carousel-confian">
-			    <div><img class="imagen-confian" alt="Mavimovil" src="assets/web/img/clientes_01.png"  height="50"></div>
-			    <div><img class="imagen-confian" alt="Mavimovil" src="assets/web/img/clientes-02.png"  height="50"></div>
-			    <div><img class="imagen-confian" alt="Mavimovil" src="assets/web/img/clientes-03.png"  height="50"></div>
-			    <div><img class="imagen-confian" alt="Mavimovil" src="assets/web/img/clientes-04.png"  height="50"></div>
-			    <div><img class="imagen-confian" alt="Mavimovil" src="assets/web/img/clientes-05.png"  height="50"></div>
-			    <div><img class="imagen-confian" alt="Mavimovil" src="assets/web/img/clientes-06.png"  height="50"></div>
+			    <div><img class="imagen-confian" alt="KAUFMANN" src="assets/web/img/clientes_01.png"  height="50"></div>
+			    <div><img class="imagen-confian" alt="AUTOFIN" src="assets/web/img/clientes-02.png"  height="50"></div>
+			    <div><img class="imagen-confian" alt="MAVIGPS" src="assets/web/img/clientes-03.png"  height="50"></div>
+			    <div><img class="imagen-confian" alt="MALLPLAZA" src="assets/web/img/clientes-04.png"  height="50"></div>
+			    <div><img class="imagen-confian" alt="BKSPA" src="assets/web/img/clientes-05.png"  height="50"></div>
+			    <div><img class="imagen-confian" alt="REDSALUD" src="assets/web/img/clientes-06.png"  height="50"></div>
+			</div>
+
+
+			<!-- version responsive-->
+			<div class="carousel-confian2">
+				<div class="row">
+					<div class="col-sm-4 col-sm-offset-4 ">
+
+					    <div><img class="imagen-confian2" alt="KAUFMANN" src="assets/web/img/clientes_01.png"  height="50"></div>
+					    <div><img class="imagen-confian2" alt="AUTOFIN" src="assets/web/img/clientes-02.png"  height="50"></div>
+					    <div><img class="imagen-confian2" alt="MAVIGPS" src="assets/web/img/clientes-03.png"  height="50"></div>
+					    <div><img class="imagen-confian2" alt="MALLPLAZA" src="assets/web/img/clientes-04.png"  height="50"></div>
+					    <div><img class="imagen-confian2" alt="BKSPA" src="assets/web/img/clientes-05.png"  height="50"></div>
+					    <div><img class="imagen-confian2" alt="REDSALUD" src="assets/web/img/clientes-06.png"  height="50"></div>
+
+					</div>
+				</div>
 			</div>
 			
 		</div>	
