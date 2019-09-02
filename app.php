0e6c374ad67b0224f5a4b55b2c4f194d04c630d9
Scarlet Villasana <s.claret.v@gmail.com>
2019-09-01 21:01:52 -0400

    version table casi ok

--- app.php
+++ app.php
@@ -1,32 +1,43 @@
 <div class="container">
 	<div class="row">
 		<div class="col-md-7 col-md-offset-0 col-xs-10 col-xs-offset-1">
-			<div>
+			<div class="img-app">
 				<img src="assets/web/img/apps-fondo-2.svg" class="fondo4" alt=""  aria-hidden=true>
 				<img src="assets/web/img/fondo-verde.svg" alt=""  class="fondo5" aria-hidden=true>
 
-				<div class="animated  fast3 efx" data-up="fadeInLeft" data-down="fadeOut "  style="z-index: 2">
-					<img src="assets/web/img/app-3.png" alt=""  class="fondo6" aria-hidden=true>
+				<div class="celus-app">
+					<div class="animated  fast3 efx" data-up="fadeInLeft" data-down="fadeOut "  style="z-index: 2">
+						<img src="assets/web/img/app-3.png" alt=""  class="fondo6" aria-hidden=true>
+					</div>
+					
+					<div class="animated fast2 efx" data-up="fadeInLeft" data-down="fadeOut "  style="z-index: 3">
+						<img src="assets/web/img/app-2.png" alt=""  class="fondo7" aria-hidden=true>
+					</div>
+
+					<div class="animated fast efx"  data-up="fadeInLeft" data-down="fadeOut " style="z-index: 4">
+						<img src="assets/web/img/app-1.png" alt=""  class="fondo8" aria-hidden=true>
+					</div>
 				</div>
 				
-				<div class="animated fast2 efx" data-up="fadeInLeft" data-down="fadeOut "  style="z-index: 3">
-					<img src="assets/web/img/app-2.png" alt=""  class="fondo7" aria-hidden=true>
-				</div>
-
-			
-
-				<div class="animated fast efx"  data-up="fadeInLeft" data-down="fadeOut " style="z-index: 4">
-					<img src="assets/web/img/app-1.png" alt=""  class="fondo8" aria-hidden=true>
-				</div>
 			</div>
 		</div>
 
 		<div class="col-md-5 col-md-offset-0 col-xs-6 col-xs-offset-3">
-			<div class="text-right">
-				<img alt="Desarrollo de Apps" src="assets/web/img/IconoApp.svg" width="300" height="200" class="app-icono efx" data-up="fadeInRight" data-down="fadeOut ">
+			<div class="row">
+				<div class="col-md-12 col-xs-4">
+					<div class="text-right">
+						<img alt="Desarrollo de Apps" src="assets/web/img/IconoApp.svg" width="300" height="200" class="app-icono efx" data-up="fadeInRight" data-down="fadeOut ">
+					</div>
+				</div>
+				<div class="col-xs-8 hidden-md hidden-lg">
+					<div class="text-left">
+						<h1 class="heading-2 efx" data-up="fadeInRight" data-down="fadeOut ">Desarrollo de Apps</h1>
+					</div>
+				</div>
 			</div>
+			
 			<div>
-				<h1 class="heading-2 efx" data-up="fadeInRight" data-down="fadeOut ">Desarrollo de Apps</h1>
+				<h1 class="heading-2 hidden-sm hidden-xs efx" data-up="fadeInRight" data-down="fadeOut ">Desarrollo de Apps</h1>
 				<h3 class="heading-3 efx" data-up="fadeInRight" data-down="fadeOut ">Aplicaciones móviles nativas</h3>
 				<p class="paragraph-2 efx" data-up="fadeInRight" data-down="fadeOut ">Es una aplicación informática diseñada para ser ejecutada en teléfonos inteligentes, tabletas y otros dispositivos móviles. Las aplicaciones permiten al usuario efectuar un conjunto de tareas de cualquier tipo —profesional, de ocio, educativas, de acceso a servicios, etc.—, facilitando las gestiones o actividades a desarrollar.</p>
 			</div>
